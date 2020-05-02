023aa83fd68bb2de163bd9d5439dc01adc4f7ed1
rajeshpandalss <rajesh.panda@lssinc.com>
2020-05-03 04:22:28 +0530

    Initial commit

---
+++ app/src/main/java/tech/unmashed/musiclauncher/MainActivity.java
@@ -0,0 +1,34 @@
+package tech.unmashed.musiclauncher;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intent launchIntent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        this.launchIntent = getPackageManager().getLaunchIntentForPackage("com.google.android.apps.youtube.music"); // replace with your intended app
+        if (this.launchIntent != null) {
+            startActivity(this.launchIntent);//null pointer check in case package name was not found
+        }
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        if (this.launchIntent == null) {
+            this.launchIntent = getPackageManager().getLaunchIntentForPackage("com.google.android.apps.youtube.music"); // replace with your intended app
+        }
+
+        if (this.launchIntent != null) {
+            startActivity(this.launchIntent);//null pointer check in case package name was not found
+        }
+    }
+}
